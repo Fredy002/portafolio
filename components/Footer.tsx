fb77ba70edb7fb6d6c488d01f75c9bfc3d20f70e
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-05-23 22:44:47 -0500

    refactor: Add external links to Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -36,7 +36,9 @@ const Footer = () => {
                             key={item.id}
                             className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 border border-black-300 rounded-xl'
                         >
+                            <a href={item.link} target='_blank' rel='noreferrer'>
                             <img src={item.img} alt={item.id} width={20} height={20}/>
+                            </a>
                         </div>
                     ))}
                 </div>
