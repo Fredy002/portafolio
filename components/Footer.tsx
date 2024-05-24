ea75b7002aea2bd4c52673f949496cef94d2ff4d
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-05-24 12:54:51 -0500

    refactor: Update Footer component to use Next.js Image component for social media icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import MagicButton from './ui/MagicButton'
 import { FaLocationArrow } from 'react-icons/fa'
 import { socialMedia } from '../data/index'
+import Image from 'next/image'
 
 const Footer = () => {
     return (
@@ -37,7 +38,7 @@ const Footer = () => {
                             className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 border border-black-300 rounded-xl'
                         >
                             <a href={item.link} target='_blank' rel='noreferrer'>
-                                <img src={item.img} alt={item.id} width="20" height="20" />
+                                <Image src={item.img} alt={item.id} width={20} height={20} />
                             </a>
                         </div>
                     ))}
