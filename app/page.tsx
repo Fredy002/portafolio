eac9ea7baa55c841c3813ba470474122d8642b6e
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-05-23 21:26:03 -0500

    refactor: Add Experiencie component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Experiencie from "@/components/Experiencie";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -16,6 +17,7 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects/>
+        <Experiencie/>
       </div>
     </main>
   );
